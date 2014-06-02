d022e01016e51d108df946ea4d373db77e9fa5da
Milan Felix Sulc <rkfelix@gmail.com>
2014-06-02 21:37:50 +0200

    Examples: update examples

--- tests/examples/TestDispatcher.php
+++ tests/examples/TestDispatcher.php
@@ -6,19 +6,18 @@ use MobilniPlatby\Dispatcher;
 use MobilniPlatby\Request\AbstractRequest;
 use MobilniPlatby\Response\AbstractResponse;
 use MobilniPlatby\Response\Response;
-use Nette\Object;
 
-class TestDispatcher extends Object implements Dispatcher
+class TestDispatcher implements Dispatcher
 {
 
-	/**
-	 * @param AbstractRequest $request
-	 * @param AbstractResponse $response
-	 * @return AbstractResponse
-	 */
-	public function dispatch(AbstractRequest $request)
-	{
-		return new Response("Tohle je super!", "FREE12039123");
-	}
+    /**
+     * @param AbstractRequest $request
+     * @param AbstractResponse $response
+     * @return AbstractResponse
+     */
+    public function dispatch(AbstractRequest $request)
+    {
+        return new Response("Tohle je super!", "FREE12039123");
+    }
 
 }
