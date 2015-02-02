d15dcef03399fc244a8dd3483ece18848da068e1
Milan Felix Sulc <rkfelix@gmail.com>
2015-02-02 21:32:14 +0100

    Tests: pridano gitignore

--- tests/tester.bat
+++ tests/tester.bat
@@ -1,3 +1,3 @@
 @echo off
-%CD%\..\vendor\bin\tester.bat -w %CD%\MobilniPlatby -s -j 40 -log %CD%\mobilniplatby.log
+%CD%\..\vendor\bin\tester.bat -w %CD%\MobilniPlatby -s -j 40 -log %CD%\tester.log
 rmdir %CD%\tmp /Q /S
