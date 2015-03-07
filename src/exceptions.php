307946c438b89eaa84f5386bdf91e25d2a8914ca
Milan Felix Sulc <rkfelix@gmail.com>
2015-03-07 12:33:56 +0100

    Exceptions: added phpdocs

--- src/exceptions.php
+++ src/exceptions.php
@@ -2,25 +2,44 @@
 
 namespace MobilniPlatby;
 
+use RuntimeException;
+
 /**
- * Exceptions
+ * SmsException - parent for all exceptions in this package
  *
  * @version 1.0.0
  * @author Milan Felix Sulc <rkfelix@gmail.com>
  */
-
-class SmsException extends \RuntimeException
+class SmsException extends RuntimeException
 {
 }
 
+/**
+ * DispatcherException
+ *
+ * @version 1.0.0
+ * @author Milan Felix Sulc <rkfelix@gmail.com>
+ */
 class DispatcherException extends SmsException
 {
 }
 
+/**
+ * RequestException
+ *
+ * @version 1.0.0
+ * @author Milan Felix Sulc <rkfelix@gmail.com>
+ */
 class RequestException extends SmsException
 {
 }
 
+/**
+ * ResponseException
+ *
+ * @version 1.0.0
+ * @author Milan Felix Sulc <rkfelix@gmail.com>
+ */
 class ResponseException extends SmsException
 {
 }
